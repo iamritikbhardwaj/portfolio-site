016153ad7aada2d8a513611512bb93d37ae7616d
Ritik Singh <ritiklrt2@gmail.com>
2025-03-25 19:08:13 +0530

    made some changes in design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,37 +7,38 @@ import {
   faLinkedin,
   faWhatsapp,
   faGithub,
+  faAws,
+  faEthereum,
 } from "@fortawesome/free-brands-svg-icons";
 import { faCss3 } from "@fortawesome/free-brands-svg-icons/faCss3";
 import { faHtml5 } from "@fortawesome/free-brands-svg-icons/faHtml5";
 import { faLinux } from "@fortawesome/free-brands-svg-icons/faLinux";
 import { faCode, faEnvelope, faLeaf } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="text-yellow-400">
+    <div className="text-cyan-600">
       <header className="text-gray-300">
-        <nav id="nav" aria-label="Main Navigation ">
-          <div className=" align-items-center p-2 flex justify-content-around">
+        <nav id="nav" aria-label="">
+          <div className="justify-center p-2 flex justify-content-around">
             <div className="hidden md:block">
-              <p>
-                <FontAwesomeIcon icon={faCode} />
-              </p>
+              <FontAwesomeIcon icon={faCode} />
             </div>
             <div className="hidden md:block align-middle">
-              <ul className="nav-links mx-4 sm:mx-20 sm:text-2xl border-2 rounded-full p-3">
+              <ul className="flex justify-around mx-4 sm:mx-20 sm:text-2xl border-2 rounded-full p-6">
                 <li id="li1">
-                  <a href="#home">Home</a>
+                  <Link href="#home">Home</Link>
                 </li>
                 <li id="li2">
-                  <a href="#about">About</a>
+                  <Link href="#about">About</Link>
                 </li>
                 <li id="li3">
-                  <a href="#work">Work</a>
+                  <Link href="#work">Work</Link>
                 </li>
                 <li id="li4">
-                  <a href="#contact">Contact</a>
+                  <Link href="#contact">Contact</Link>
                 </li>
               </ul>
             </div>
@@ -45,7 +46,7 @@ export default function Home() {
               id="socials"
               className=" hidden md:block text-2xl text-yellow-600"
             >
-              <a
+              <Link
                 id="nava"
                 className="m-1"
                 href="https://www.linkedin.com/in/ritik-singh-10b333227"
@@ -56,14 +57,15 @@ export default function Home() {
                   icon={faLinkedin}
                   className="text-yellow-800"
                 />
-              </a>
-              <a
+              </Link>
+              <Link
                 href="https://wa.me/9119060487
           "
               >
                 <FontAwesomeIcon icon={faWhatsapp} />
-              </a>
-              <a
+              </Link>
+
+              <Link
                 id="nava1"
                 className="m-1 text-2xl"
                 href="https://github.com/iamritikbhardwaj"
@@ -71,10 +73,10 @@ export default function Home() {
                 title="GitHub Profile"
               >
                 <i className="fab fa-github"></i>
-              </a>
-              <a href="mailto:ritiklrt2@gmail.com">
+              </Link>
+              <Link href="mailto:ritiklrt2@gmail.com">
                 <FontAwesomeIcon icon={faEnvelope} />
-              </a>
+              </Link>
             </div>
           </div>
         </nav>
@@ -82,11 +84,12 @@ export default function Home() {
 
       {/* <!-- Home Section --> */}
       <section id="home">
+        {/* <img src="/https://avatars.githubusercontent.com/u/87214866?v=4" alt="ritik" className="text-gray-300" /> */}
         <h1 className="text-4xl text-gray-300">
           Ritik Singh | Full Stack Developer
         </h1>
         <p>
-          I speak in <code>&lt;code&gt;</code> so you don't have to.
+          I speak in <code className="text-green-600 text-2xl">&lt;code&gt;</code> so you don't have to.
         </p>
       </section>
 
@@ -104,7 +107,7 @@ export default function Home() {
       {/* <!-- Work Section --> */}
       <section id="work">
         <h2 className="text-4xl text-gray-300">Featured Projects</h2>
-        <div className="bg-gray-400 flex flex-row justify-center p-2 mx-20">
+        <div className="bg-slate-800 flex flex-row justify-center p-2 mx-20">
           <div className="flex flex-col justify-center mx-20">
             <h3>Project 1: Scroll Animation</h3>
             <p>
@@ -112,20 +115,20 @@ export default function Home() {
               transitions.
             </p>
           </div>
-          <a
-            href="scrollAni.html"
+          <Link
+            href="./scrollAni.html"
             title="Click to open scroll animation project"
           >
-            <div className="hidden sm:block mx-20">
+            <div className="hidden shadow-xs sm:block mx-20">
               <iframe
                 src="scrollAni.html"
                 title="Scroll Animation Project Preview"
               ></iframe>
             </div>
-          </a>
+          </Link>
         </div>
-        <div className="container">
-          <div className="flex justify-center p-2">
+        <div className="text-lg mt-6">
+          <div className="flex justify-center mt-2 p-2">
             <img
               src="https://img.icons8.com/ios-filled/50/000000/sword.png"
               alt="Icon representing Weapons for development tools"
@@ -135,41 +138,22 @@ export default function Home() {
               My Weapons
             </h3>
           </div>
-          <div className="logos flex flex-row justify-center text-gray-300 spacing-2 p-2">
-            <i className="fa-brands fa-java text-8xl align-self-center mx-2"></i>
+          <div className="flex justify-around-center text-gray-300  p-2">
+            <FontAwesomeIcon icon={faJs} className="text-yellow-300"/>
 
-            <img
-              src="https://img.icons8.com/color/100/000000/mongodb.png"
-              alt="MongoDB Logo"
-            />
+            <FontAwesomeIcon icon={faReact} className="text-cyan-500"/>
 
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 30 30"
-              width="30"
-              height="30"
-            >
-              {/* <!-- SVG content here --> */}
-            </svg>
-            <FontAwesomeIcon icon={faJs} />
-
-            <FontAwesomeIcon icon={faHtml5} />
-
-            <FontAwesomeIcon icon={faCss3} />
-
-            <FontAwesomeIcon icon={faReact} />
-            
-            <FontAwesomeIcon icon={faNode} />
+            <FontAwesomeIcon icon={faNode} className="text-green-500"/>
 
             <FontAwesomeIcon icon={faJava} />
 
-            <FontAwesomeIcon icon={faLeaf} />
-
-            <FontAwesomeIcon icon={faGit} />
+            <FontAwesomeIcon icon={faGit} className="text-orange-500"/>
 
             <FontAwesomeIcon icon={faLinux} />
 
-            {/* <FontAwesomeIcon icon={faMySql} /> */}
+            <FontAwesomeIcon icon={faEthereum} className="text-purple-400" />
+
+            <FontAwesomeIcon icon={faAws} />
 
             {/* add tailwind as well */}
           </div>
@@ -180,30 +164,27 @@ export default function Home() {
       <section id="contact">
         <h2 className="text-4xl text-gray-300">Contact</h2>
         <p>Get in touch if you want to work together!</p>
-        <div className="social-icons text-2xl">
-          <a href="mailto:ritiklrt2@gmail.com">
-            <FontAwesomeIcon icon={faEnvelope} className="text-sm" />
-          </a>
-          <a
+        <div className="flex justify-center text-gray-300 spacing-2 p-2">
+          <Link href="mailto:ritiklrt2@gmail.com">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </Link>
+          <Link
             href="https://www.linkedin.com/in/ritik-singh-10b333227"
             target="_blank"
             title="LinkedIn Profile"
           >
             <FontAwesomeIcon icon={faLinkedin} />
-          </a>
-          <a
-            href="https://wa.me/9119060487
-          "
-          >
+          </Link>
+          <Link href="https://wa.me/9119060487">
             <FontAwesomeIcon icon={faWhatsapp} />
-          </a>
-          <a
+          </Link>
+          <Link
             href="https://github.com/iamritikbhardwaj"
             target="_blank"
             title="GitHub Profile"
           >
             <FontAwesomeIcon icon={faGithub} />
-          </a>
+          </Link>
         </div>
       </section>
 
@@ -213,7 +194,7 @@ export default function Home() {
         <p>Click the download button to get a copy of my resume.</p>
         <div className="flex flex-row justify-center">
           <iframe
-            src="/portfolio-site/public/Resume.pdf"
+            src="./Resume.pdf"
             width="600"
             height="400"
             title="Resume PDF Viewer"
