a92d475a0c990f25d2bba14eadc161e7c529d606
Ritik Singh <87214866+iamritikbhardwaj@users.noreply.github.com>
2025-06-11 01:10:34 +0530

    bhang bhosda

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: '/portfolio',
   trailingSlash: true,
+  assetPrefix: '/portfolio/',
 };
 
 export default nextConfig;
